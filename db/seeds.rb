be4a6dff8306d79a740b4efa0d2d85e9c4ade0ac
jcfasto <jcfausto@gmail.com>
2018-02-16 22:18:34 +0100

    Adding seed data for testing purposes

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,37 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# Some test messages
+
+message = Message.create!(
+		content: "Hello, I am a secret message near bauhaus berlin!", 
+		latitude: 52.5311535,
+		longitude: 13.3777432
+)
+
+message = Message.create!(
+		content: "Hello, I am a secret message!", 
+		latitude: 52.5305726,
+		longitude: 13.3769815
+)
+
+message = Message.create!(
+		content: "Hello, I am a secret message near the mauerpark!", 
+		latitude: 52.5386875,
+		longitude: 13.3749452
+)
+
+message = Message.create!(
+		content: "Hello, I am a secret message near the olympia stadium!", 
+		latitude: 52.5152296,
+		longitude: 13.2797457
+)
+
+message = Message.create!(
+		content: "Hello, I am a secret message near potsdam!", 
+		latitude: 52.4282784,
+		longitude: 12.8869844
+)
+
+message = Message.create!(
+		content: "Hello, I am a secret message in France near le jardin du luxemburg!", 
+		latitude: 48.8567835,
+		longitude: 2.3024531
+)
