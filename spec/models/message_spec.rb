9e1775b60f81505d03e27fa3e5a5a773f33726b3
jcfasto <jcfausto@gmail.com>
2018-02-16 23:07:42 +0100

    Added more tests to Message spec

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -6,8 +6,27 @@ RSpec.describe Message, type: :model do
 	
 	# Validations
 	describe "validations" do
-		it { should validate_presence_of(:content) }
-		it { should validate_presence_of(:latitude) }
-		it { should validate_presence_of(:longitude) }
+		describe "presence" do
+			it { should validate_presence_of(:content) }
+			it { should validate_presence_of(:latitude) }
+			it { should validate_presence_of(:longitude) }
+		end
+
+		describe "content lenght" do
+			context "lenght is valid" do
+				it "creates the message" do
+					expect(subject)
+				end
+			end
+
+			context "when length is invalid" do
+				subject { described_class.create(content: Faker::Lorem::sentence(word_count=100), latitude: 1, longitude: 1) }
+
+				it "does not create the message" do
+					expect(subject.valid?).to be_falsey
+				end
+			end
+
+		end
 	end
 end
